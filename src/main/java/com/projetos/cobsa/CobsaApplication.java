9f26f2d08c0762168445a1310291e338f1ca8478
jcr04 <joaocarlosreboucas15@gmail.com>
2023-11-07 18:55:16 -0300

    add sistem

---
+++ src/main/java/com/projetos/cobsa/CobsaApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.cobsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/cobsa/CobsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.cobsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
